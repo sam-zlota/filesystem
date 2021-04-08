6e62a8bdeb6f9892d64d286e26e8663d5ae5df15
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-07 23:11:51 -0400

    debugging

--- source/nufs.c
+++ source/nufs.c
@@ -12,16 +12,17 @@
 
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
+#include "pages.h"
 
-// implementation for: man 2 access
-// Checks if a file exists.
-int
-nufs_access(const char *path, int mask)
-{
-    int rv = 0;
-    printf("access(%s, %04o) -> %d\n", path, mask, rv);
-    return rv;
-}
+// // implementation for: man 2 access
+// // Checks if a file exists.
+// int
+// nufs_access(const char *path, int mask)
+// {
+//     int rv = 0;
+//     printf("access(%s, %04o) -> %d\n", path, mask, rv);
+//     return rv;
+// }
 
 // implementation for: man 2 stat
 // gets an object's attributes (type, permissions, size, etc)
@@ -46,27 +47,27 @@ nufs_getattr(const char *path, struct stat *st)
     return rv;
 }
 
-// implementation for: man 2 readdir
-// lists the contents of a directory
-int
-nufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-             off_t offset, struct fuse_file_info *fi)
-{
-    struct stat st;
-    int rv;
+// // implementation for: man 2 readdir
+// // lists the contents of a directory
+// int
+// nufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+//              off_t offset, struct fuse_file_info *fi)
+// {
+//     struct stat st;
+//     int rv;
 
-    rv = nufs_getattr("/", &st);
-    assert(rv == 0);
+//     rv = nufs_getattr("/", &st);
+//     assert(rv == 0);
 
-    filler(buf, ".", &st, 0);
+//     filler(buf, ".", &st, 0);
 
-    rv = nufs_getattr("/hello.txt", &st);
-    assert(rv == 0);
-    filler(buf, "hello.txt", &st, 0);
+//     rv = nufs_getattr("/hello.txt", &st);
+//     assert(rv == 0);
+//     filler(buf, "hello.txt", &st, 0);
 
-    printf("readdir(%s) -> %d\n", path, rv);
-    return 0;
-}
+//     printf("readdir(%s) -> %d\n", path, rv);
+//     return 0;
+// }
 
 // mknod makes a filesystem object like a file or directory
 // called for: man 2 open, man 2 link
@@ -96,47 +97,47 @@ nufs_unlink(const char *path)
     return rv;
 }
 
-int
-nufs_link(const char *from, const char *to)
-{
-    int rv = -1;
-    printf("link(%s => %s) -> %d\n", from, to, rv);
-	return rv;
-}
+// int
+// nufs_link(const char *from, const char *to)
+// {
+//     int rv = -1;
+//     printf("link(%s => %s) -> %d\n", from, to, rv);
+// 	return rv;
+// }
 
-int
-nufs_rmdir(const char *path)
-{
-    int rv = -1;
-    printf("rmdir(%s) -> %d\n", path, rv);
-    return rv;
-}
+// int
+// nufs_rmdir(const char *path)
+// {
+//     int rv = -1;
+//     printf("rmdir(%s) -> %d\n", path, rv);
+//     return rv;
+// }
 
-// implements: man 2 rename
-// called to move a file within the same filesystem
-int
-nufs_rename(const char *from, const char *to)
-{
-    int rv = -1;
-    printf("rename(%s => %s) -> %d\n", from, to, rv);
-    return rv;
-}
+// // implements: man 2 rename
+// // called to move a file within the same filesystem
+// int
+// nufs_rename(const char *from, const char *to)
+// {
+//     int rv = -1;
+//     printf("rename(%s => %s) -> %d\n", from, to, rv);
+//     return rv;
+// }
 
-int
-nufs_chmod(const char *path, mode_t mode)
-{
-    int rv = -1;
-    printf("chmod(%s, %04o) -> %d\n", path, mode, rv);
-    return rv;
-}
+// int
+// nufs_chmod(const char *path, mode_t mode)
+// {
+//     int rv = -1;
+//     printf("chmod(%s, %04o) -> %d\n", path, mode, rv);
+//     return rv;
+// }
 
-int
-nufs_truncate(const char *path, off_t size)
-{
-    int rv = -1;
-    printf("truncate(%s, %ld bytes) -> %d\n", path, size, rv);
-    return rv;
-}
+// int
+// nufs_truncate(const char *path, off_t size)
+// {
+//     int rv = -1;
+//     printf("truncate(%s, %ld bytes) -> %d\n", path, size, rv);
+//     return rv;
+// }
 
 // this is called on open, but doesn't need to do much
 // since FUSE doesn't assume you maintain state for
@@ -168,25 +169,25 @@ nufs_write(const char *path, const char *buf, size_t size, off_t offset, struct 
     return rv;
 }
 
-// Update the timestamps on a file or directory.
-int
-nufs_utimens(const char* path, const struct timespec ts[2])
-{
-    int rv = -1;
-    printf("utimens(%s, [%ld, %ld; %ld %ld]) -> %d\n",
-           path, ts[0].tv_sec, ts[0].tv_nsec, ts[1].tv_sec, ts[1].tv_nsec, rv);
-	return rv;
-}
+// // Update the timestamps on a file or directory.
+// int
+// nufs_utimens(const char* path, const struct timespec ts[2])
+// {
+//     int rv = -1;
+//     printf("utimens(%s, [%ld, %ld; %ld %ld]) -> %d\n",
+//            path, ts[0].tv_sec, ts[0].tv_nsec, ts[1].tv_sec, ts[1].tv_nsec, rv);
+// 	return rv;
+// }
 
-// Extended operations
-int
-nufs_ioctl(const char* path, int cmd, void* arg, struct fuse_file_info* fi,
-           unsigned int flags, void* data)
-{
-    int rv = -1;
-    printf("ioctl(%s, %d, ...) -> %d\n", path, cmd, rv);
-    return rv;
-}
+// // Extended operations
+// int
+// nufs_ioctl(const char* path, int cmd, void* arg, struct fuse_file_info* fi,
+//            unsigned int flags, void* data)
+// {
+//     int rv = -1;
+//     printf("ioctl(%s, %d, ...) -> %d\n", path, cmd, rv);
+//     return rv;
+// }
 
 void
 nufs_init_ops(struct fuse_operations* ops)
