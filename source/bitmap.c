3887e1811f4f80055333da23a4b6c684a46724be
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-07 23:49:42 -0400

    impl bitmap

--- source/bitmap.c
+++ source/bitmap.c
@@ -1,10 +1,44 @@
-// based on cs3650 starter code
 
-#ifndef BITMAP_H
-#define BITMAP_H
+#include <stdint.h>
 
-int bitmap_get(void* bm, int ii);
-void bitmap_put(void* bm, int ii, int vv);
-void bitmap_print(void* bm, int size);
+int pow(int exp) {
 
-#endif
+    int res = 1;
+    while(exp) {
+        res*=2;
+        exp--;
+    }
+    return res;
+
+    
+}
+int bitmap_get(void* bm, int ii) {
+    int remainder = ii % 8; 
+    int bytes = ii / 8;
+    uint8_t val = *(uint8_t*)(bm + bytes);
+    return val & pow(remainder);
+
+}
+
+
+
+void bitmap_put(void* bm, int ii, int vv) {
+    int remainder = ii % 8; 
+    int bytes = ii / 8;
+    uint8_t* val = (uint8_t*)(bm + bytes);
+    if(vv) 
+        *val = *val | pow(remainder);
+    else
+        *val = *val & pow(remainder);
+}
+
+
+void bitmap_print(void* bm, int size) {
+
+    for(int ii = 0; ii < size; ii++ ) {
+
+        printf("%d : %d\n", ii, bitmap_get(bm, ii));
+    }
+
+}
+
