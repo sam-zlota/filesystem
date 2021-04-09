0cf02d2939e9ff5a68f6a045873f770106f525c7
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-09 09:03:40 -0400

    saving work

--- source/bitmap.c
+++ source/bitmap.c
@@ -1,34 +1,42 @@
 
 #include <stdint.h>
 
-int powr(int exp) {
-
+int mask(int rem) {
+    assert(rem < 8);
+    //rem, 0 ==> 1000_0000 ==> 128
+    //rem, 1 ==> 0100_0000 ==> 64
+    //...
+    //rem 6 ==> 0000_0010 ==> 2
+    //rem 7 ==> 0000_0001 ==> 1
+    int exp = 8 - rem + 1;
     int res = 1;
     while(exp) {
         res*=2;
         exp--;
     }
     return res;
-
-    
 }
 
+
 int bitmap_get(void* bm, int ii) {
+    assert(ii < 256);
     int remainder = ii % 8; 
     int bytes = ii / 8;
     uint8_t val = *(uint8_t*)(bm + bytes);
-    return val & powr(remainder);
+    return val & mask(remainder);
 
 }
 
 void bitmap_put(void* bm, int ii, int vv) {
+    assert(ii < 256);
+    assert(vv == 0 || vv == 1);
     int remainder = ii % 8; 
     int bytes = ii / 8;
     uint8_t* val = (uint8_t*)(bm + bytes);
     if(vv) 
-        *val = *val | powr(remainder);
+        *val = *val | mask(remainder);
     else
-        *val = *val & powr(remainder);
+        *val = *val & mask(remainder);
 }
 
 
