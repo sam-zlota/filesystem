1de146acb37934d01ae02e4f3d01fda4260ec894
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-09 14:46:54 -0400

    saving

--- source/bitmap.c
+++ source/bitmap.c
@@ -3,28 +3,36 @@
 #include <stdio.h>
 #include <assert.h>
 
-int mask(int rem) {
-  assert(rem < 8);
-  // rem, 0 ==> 1000_0000 ==> 128
-  // rem, 1 ==> 0100_0000 ==> 64
-  //...
-  // rem 6 ==> 0000_0010 ==> 2
-  // rem 7 ==> 0000_0001 ==> 1
-  int exp = 8 - rem + 1;
-  int res = 1;
-  while (exp) {
-    res *= 2;
-    exp--;
-  }
-  return res;
-}
+// int mask(int rem) {
+//   assert(rem < 8);
+//   // rem, 0 ==> 1000_0000 ==> 128
+//   // rem, 1 ==> 0100_0000 ==> 64
+//   //...
+//   // rem 6 ==> 0000_0010 ==> 2
+//   // rem 7 ==> 0000_0001 ==> 1
+//   int exp = 8 - rem + 1;
+//   int res = 1;
+//   while (exp) {
+//     res *= 2;
+//     exp--;
+//   }
+//   return res;
+// }
+
+
+// rem, 0 ==> 1000_0000 ==> 128
+// rem, 1 ==> 0100_0000 ==> 64
+// ...
+// rem 6 ==> 0000_0010 ==> 2
+// rem 7 ==> 0000_0001 ==> 1
+static int mask[8] = {128, 64, 32, 16, 8, 4, 2, 1};
 
 int bitmap_get(void *bm, int ii) {
   assert(ii < 256);
   int remainder = ii % 8;
   int bytes = ii / 8;
   uint8_t val = *(uint8_t *)(bm + bytes);
-  return val & mask(remainder);
+  return val & mask[remainder];
 }
 
 void bitmap_put(void *bm, int ii, int vv) {
@@ -34,9 +42,9 @@ void bitmap_put(void *bm, int ii, int vv) {
   int bytes = ii / 8;
   uint8_t *val = (uint8_t *)(bm + bytes);
   if (vv)
-    *val = *val | mask(remainder);
+    *val = *val | mask[remainder];
   else
-    *val = *val & mask(remainder);
+    *val = *val & mask[remainder];
 }
 
 void bitmap_print(void *bm, int size) {
