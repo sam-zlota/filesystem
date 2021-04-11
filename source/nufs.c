e7551f7f6d905924bf3f3016cc9e95f819053202
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-10 20:31:31 -0400

    saving work

--- source/nufs.c
+++ source/nufs.c
@@ -143,6 +143,7 @@ int nufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
         break;
       } else {
         // direntry must be initialized
+        struct stat st;
         rv = nufs_getattr(strcat(direntry_arr[ii].name, "/"), &st);
         assert(rv == 0);
         filler(buf, strcat(direntry_arr[ii].name, "/"), &st, 0);
