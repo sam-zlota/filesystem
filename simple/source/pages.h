18df56ab58753d80cacfe21e123c5e0dfc8ced92
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-13 14:11:55 -0400

    restructuring

---
+++ simple/source/pages.h
@@ -0,0 +1,18 @@
+// based on cs3650 starter code
+
+#ifndef PAGES_H
+#define PAGES_H
+
+#include <stdio.h>
+
+void pages_init(const char* path);
+void pages_free();
+void* pages_get_page(int pnum);
+void* get_pages_bitmap();
+void* get_inode_bitmap();
+int alloc_page();
+int alloc_inum();
+void free_page(int pnum);
+void free_inode(int inum);
+
+#endif
