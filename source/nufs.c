08f03f7e202e32bfdfd506a86b120e8308d28e08
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-13 00:25:10 -0400

    saving work

--- source/nufs.c
+++ source/nufs.c
@@ -296,6 +296,7 @@ int nufs_unlink(const char *path) {
     desired_inode->refs--;
     if (desired_inode->refs == 0) {
       printf("ERASING\n");
+      assert(desired_direntry.inum != 0);
       free_page(desired_page_num);
       free_inode(desired_inum);
       memset(&desired_direntry, 0, sizeof(direntry));
