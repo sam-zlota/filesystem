17fd41dd039cbb4c9c2138d3431e4c07f837ca8c
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-12 23:15:13 -0400

    saving work

--- source/nufs.c
+++ source/nufs.c
@@ -144,7 +144,7 @@ int nufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
   if (strcmp(path, "/") == 0) {
     rv = nufs_getattr("/", &st);
     assert(rv == 0);
-    filler(buf, "/", &st, 0);
+    filler(buf, ".", &st, 0);
     // buf += strlen("/");
     void *root_block = pages_get_page(ROOT_PNUM);
     direntry *direntry_arr = (direntry *)root_block;
@@ -171,7 +171,7 @@ int nufs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
         // printf("NO SEG: %ld\n", ii);
 
         //"/name"TODO: make sure null terminated and have / in front
-        filler(buf, direntry_arr[ii].name, &st, 0);
+        filler(buf, get_filename_from_path(direntry_arr[ii].name), &st, 0);
         // printf("read %s from dir\n", direntry_arr[ii].name);
       }
     }
