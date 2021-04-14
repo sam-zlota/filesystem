0fbda042ca05b455e2bec412248bc46156c1029a
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-14 16:20:06 -0400

    saving

--- advanced/source/directory.c
+++ advanced/source/directory.c
@@ -2,6 +2,7 @@
 #define DIR_NAME 48
 
 #include "directory.h"
+
 #include "inode.h"
 #include "pages.h"
 #include "slist.h"
@@ -21,24 +22,21 @@ int directory_delete(inode* dd, const char* name);
 slist* directory_list(const char* path);
 void print_directory(inode* dd);*/
 
-// Returns inode for the given file name in the given directory 
+// Returns inode for the given file name in the given directory
 // Returns -1 if we can't find it
-int directory_lookup(inode* dd, const char* name)
-{   
-    direntry* entries = (direntry*)pages_get_page(dd->ptrs[0] + 2);
+int directory_lookup(inode* dd, const char* name) {
+  direntry* entries = (direntry*)pages_get_page(dd->ptrs[0] + 2);
 
-    int ii = 0;
+  int ii = 0;
 
-    while (ii < min(dd->size, PAGE_SIZE))
-    {
-        if (strcmp(entries[ii].name, name) == 0)
-        {
-          return entries[ii].inum;
-        }
-        ii++;
+  while (ii < min(dd->size, PAGE_SIZE)) {
+    if (strcmp(entries[ii].name, name) == 0) {
+      return entries[ii].inum;
     }
+    ii++;
+  }
 
-    return -1;
+  return -1;
 }
 
 // Returns the parent of this path
@@ -47,15 +45,13 @@ int tree_lookup(const char* path) {
   int curr_dir = 0;
 
   // This means that we are at the root node
-  if (delim_path->next == NULL)
-  {
+  if (delim_path->next == NULL) {
     return 0;
   }
 
-  // Loop through the deliminated path, terminating only when you are the parent of the last
-  // node in the list
-  while (delim_path->next->next)
-  {
+  // Loop through the deliminated path, terminating only when you are the parent
+  // of the last node in the list
+  while (delim_path->next->next) {
     curr_dir = directory_lookup(get_inode(curr_dir), delim_path->data);
     delim_path = delim_path->next;
   }
@@ -64,21 +60,34 @@ int tree_lookup(const char* path) {
 }
 
 // Get the first free entry in the given directory block
-direntry* first_free_entry_in_block(int pnum)
-{
+direntry* first_free_entry_in_block(int pnum) {
   direntry* page = (direntry*)pages_get_page(pnum);
   return 0;
 }
 
-direntry* first_free_entry(inode* dd)
-{
-  return 0; 
-}
+direntry* first_free_entry(inode* dd) { return 0; }
 
-// Puts an inum with the given name in the given parent directory, returning the new directory's inum
-int directory_put(inode* dd, const char* name, int inum)
-{
-  
+// Puts an inum with the given name in the given parent directory, returning the
+// new directory's inum
+int directory_put(inode* dd, const char* name, int inum) {
+  // direntry *first_empty_direntry = (direntry *)&direntry_arr[ii];
+  // int first_free_inum = alloc_inum();
+  // if (first_free_inum == -1) {
+  //   return rv;
+  // }
+
+  // first_empty_direntry->inum = first_free_inum;
+  // strcat(first_empty_direntry->name, path);
+  // root_inode->size += sizeof(direntry);
+
+  // inode *new_inode = get_inode(first_empty_direntry->inum);
+
+  // new_inode->mode = 100644;
+  // new_inode->refs = 1;
+  // new_inode->size = 0;
+  // int first_free_pnum = alloc_page();
+  // if (first_free_pnum == -1) {
+  //   printf("pnum error\n");
 
   return 0;
 }
