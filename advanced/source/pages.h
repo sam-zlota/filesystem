f623c6a27bf847601df6f4fbdd3134d2ba35bfc3
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-15 21:58:12 -0400

    saving

--- advanced/source/pages.h
+++ advanced/source/pages.h
@@ -13,7 +13,6 @@ void* pages_get_page(int pnum);
 void* get_pages_bitmap();
 void* get_inode_bitmap();
 int alloc_page();
-// int alloc_inum();
 void free_page(int pnum);
 void free_inode(int inum);
 
