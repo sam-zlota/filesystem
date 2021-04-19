358169036452fe6948a9f76c5c6dffd14f712c17
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-04-19 13:00:16 -0400

    saving

--- advanced/source/nufs.c
+++ advanced/source/nufs.c
@@ -75,6 +75,7 @@ int nufs_getattr(const char *path, struct stat *st) {
   st->st_size = desired_inode->size;
   st->st_uid = getuid();
   st->st_nlink = 1;
+  st->st_ino = desired_inum;
 
 
   printf("getattr(%s) -> (%d) {mode: %04o, size: %ld}\n", path, rv, st->st_mode,
